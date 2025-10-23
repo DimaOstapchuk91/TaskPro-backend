0700c2fc1fd9eabc7f99227de75471eccde5c081
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2025-10-23 22:56:21 +0300

    change board validation

--- src/validation/boards.ts
+++ src/validation/boards.ts
@@ -1,5 +1,29 @@
 import Joi from 'joi';
 
+const imageSchema = Joi.object({
+  filename: Joi.string().required(),
+  url: Joi.string().uri().required(),
+});
+
+const backgroundSchema = Joi.object({
+  name: Joi.string().required(),
+  desk: imageSchema.required(),
+  mob: imageSchema.required(),
+  tab: imageSchema.required(),
+  thumb: imageSchema.required(),
+});
+
 export const boardSchema = Joi.object({
   title: Joi.string().min(2).max(50).required(),
+  icons: Joi.valid(
+    'icon-star',
+    'icon-container',
+    'icon-puzzle',
+    'icon-project',
+    'icon-colors',
+    'icon-hexagon',
+    'icon-lightning',
+    'icon-loading',
+  ).required(),
+  background: backgroundSchema.optional().allow(null),
 });
